5c4a9c2bf64f1b9b7f00e7443c5543dd3b024152
GussyDieTwice <gustavo.encarnacion16@tectijuana.edu.mx>
2020-10-09 17:11:03 -0700

    Primer commit

---
+++ Practicas/Practica1.scala
@@ -0,0 +1,12 @@
+//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
+//   secuencia "Luke"
+var mensaje = "Hola Luke yo soy tu padre!" 
+mensaje slice (5,9)
+
+//5. Cual es la diferencia entre value y una variable en scala?
+//El objeto asignado a Val no puede ser remplazado y el asignado a Var si puede reemplazarse
+
+//6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
+val my_tup = (2,4,5,1,2,3,3.1416,23)
+my_tup._7
+
