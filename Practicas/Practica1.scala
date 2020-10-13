1ce487c607232a56bc1428cc9472a8b3303f7b03
AdrianGiovanny <71359267+AdrianGiovanny@users.noreply.github.com>
2020-10-12 20:42:47 -0700

    Second Commit

--- Practicas/Practica1.scala
+++ Practicas/Practica1.scala
@@ -1,3 +1,24 @@
+//1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
+
+def Diame=2000
+def Radio = Diame/2
+Radioc
+
+//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+def numeroprimo(num:Int): Boolean = {
+    for(n <- Range(2, num)){
+        if(num%n == 0){
+            return false
+        }
+    }
+    return true
+}
+
+println(numeroprimo())
+
+//3. Dada la variable bird = "tweet", utiliza interpolacion de string para imprimir "Estoy ecribiendo un tweet"
+val bird = "Tweet, " + "Estoy escribiendo un Tweet!"
+
 //4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
 //   secuencia "Luke"
 var mensaje = "Hola Luke yo soy tu padre!" 
