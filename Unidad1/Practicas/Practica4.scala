2b2bfdd38e8b42474cf30a3cbdf97f6f49dbdb95
AdrianGiovanny <71359267+AdrianGiovanny@users.noreply.github.com>
2020-10-16 10:11:38 -0700

    Commit Fibonacco Function

--- Unidad1/Practicas/Practica4.scala
+++ Unidad1/Practicas/Practica4.scala
@@ -6,6 +6,14 @@ def fib1 (n:Int): Int = {
         {return fib1(n-1)+fib1(n-2)}
 }
 
+//Algoritmo 2 Versión con fórmula explícita
+n=2
+for(n <- Array.range(0,n+1))
+{
+var fibo =(Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5)
+return fibo
+}
+
 //Algoritmo 3 Versión iterativa
 def fib3 (n:Int): Int = {
     var a = 0;
@@ -19,6 +27,18 @@ def fib3 (n:Int): Int = {
         b = c;
     }
 
+    return a
+}
+
+//Algoritmo 4 Versión iterativa 2 variables 
+def fibo(n:Int): Int = {
+    var a = 0
+    var b = 1
+    for (j<-1 to n)
+    {
+        b = b+a
+        a = b-a
+    }
     return a
 }
 
@@ -40,4 +60,32 @@ def fib5 (n:Int): Int = {
             return vector(n);
         }
 
+}
+
+//Algotimo 6 Versión Divide y Vencerás
+import scala.math.pow
+def fibo(n :Double):Double={
+    if(n<=0){
+        return 0
+    }
+
+    var i = n-1
+    var aux1 =0.00
+    var aux2=1.00
+
+    var a=(aux2,aux1)
+    var b=(aux1,aux2)
+
+    while(i>0){
+        if((i%2)!=0){
+            aux1=(b._2*a._2)+(b._1*a._1)
+            aux2=(b._2*(a._2+a._1)+b._1*a._2)
+            a=(aux1,aux2)
+        }
+        aux1=pow(b._1,2)+pow(b._2,2)
+        aux2=(b._2*(2*b._1+b._2))
+        b=(aux1,aux2)
+        i=i/2
+    }
+    return (a._1+a._2)
 }
