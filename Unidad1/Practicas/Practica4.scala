9d44f8ddbab2b7262f0c8b2c0205853cba16179c
GussyDieTwice <gustavo.encarnacion16@tectijuana.edu.mx>
2020-10-15 22:15:52 -0700

    Second Commit

--- Unidad1/Practicas/Practica4.scala
+++ Unidad1/Practicas/Practica4.scala
@@ -1,5 +1,5 @@
 //Algoritmo 1 Versión recursiva descendente
-def fib1 (n:Int): Int= {
+def fib1 (n:Int): Int = {
     if (n<2)
         {return n}
     else
@@ -7,7 +7,7 @@ def fib1 (n:Int): Int= {
 }
 
 //Algoritmo 3 Versión iterativa
-def fib3 (n:Int): Int= {
+def fib3 (n:Int): Int = {
     var a = 0;
     var b = 1;
     var c = 0;
@@ -20,4 +20,24 @@ def fib3 (n:Int): Int= {
     }
 
     return a
+}
+
+//Algoritmo 5 Versión iterativa vector
+def fib5 (n:Int): Int = {
+    if (n<2)
+        {return n}
+    else 
+        {
+            var vector = new Array[Int](n+1);
+            vector(0) = 0;
+            vector(1) = 1;
+
+            for(k <- Range(2,n+1))
+            {
+                vector(k) = vector(k-1) + vector(k-2);
+            }
+
+            return vector(n);
+        }
+
 }
