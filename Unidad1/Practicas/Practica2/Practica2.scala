26bc5f7fdf23411195dfd2122b78913b78346a9c
Gustavo Encarnacion Ocampo <60414093+GussyDieTwice@users.noreply.github.com>
2020-10-25 15:13:56 -0700

    Update Practica2.scala

--- Unidad1/Practicas/Practica2/Practica2.scala
+++ Unidad1/Practicas/Practica2/Practica2.scala
@@ -2,7 +2,7 @@
 // 1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
 val lista = List("rojo", "blanco", "Negro")
 // 2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
-val lista2 = lista ::: List("verde", "amarillo", "azul")
+val lista2 = lista ::: List("verde", "amarillo", "azul", "naranja", "perla")
 // 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
 val lista3 = lista2.slice(3,6)
 // 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
