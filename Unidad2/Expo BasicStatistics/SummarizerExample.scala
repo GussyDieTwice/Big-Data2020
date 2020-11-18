a14a92d0ade015cb3e0672f98b7b552e35c5592c
AdrianGiovanny <71359267+AdrianGiovanny@users.noreply.github.com>
2020-11-18 13:30:27 -0800

    Commit Expo Example Summarizer Code

--- Unidad2/Expo BasicStatistics/SummarizerExample.scala
+++ Unidad2/Expo BasicStatistics/SummarizerExample.scala
@@ -32,23 +32,28 @@ object SummarizerExample {
       .getOrCreate()
 
     import spark.implicits._
+    //Importar Summarizer 
     import Summarizer._
 
-    // $example on$
+    // Valor data contendra dos vectores 
     val data = Seq(
       (Vectors.dense(2.0, 3.0, 5.0), 1.0),
       (Vectors.dense(4.0, 6.0, 7.0), 2.0)
     )
-
+    // Valor df para hacer referencia a "Feactures" y "weight"
     val df = data.toDF("features", "weight")
-
+    // Dos valores para la media y varianza;
+    //Selec las metricas (Mean,Variance)
     val (meanVal, varianceVal) = df.select(metrics("mean", "variance")
+    //Metodo summary que se lo aplicaremos a  Feactures y Weight y le damos un alias
       .summary($"features", $"weight").as("summary"))
+    //Seleccionamos el Summary con su respectiva metrica
       .select("summary.mean", "summary.variance")
+    // Le damos la forma en que deseamos imprimir
       .as[(Vector, Vector)].first()
-
+    // Mandamos a la impresion para mostrar los valores de la Mean y Varianza 
     println(s"with weight: mean = ${meanVal}, variance = ${varianceVal}")
-
+    // Segundo Valor sin usar el metodo Summary 
     val (meanVal2, varianceVal2) = df.select(mean($"features"), variance($"features"))
       .as[(Vector, Vector)].first()
 
