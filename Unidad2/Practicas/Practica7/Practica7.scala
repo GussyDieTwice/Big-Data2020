6afd7e0086905de29d987a4778aa66cc19bc3147
GussyDieTwice <gustavo.encarnacion16@tectijuana.edu.mx>
2020-12-05 22:13:25 -0800

    adding lr & comments

--- Unidad2/Practicas/Practica7/Practica7.scala
+++ Unidad2/Practicas/Practica7/Practica7.scala
@@ -1,5 +1,3 @@
-package org.apache.spark.examples.ml
-
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
@@ -13,31 +11,41 @@ object MultilayerPerceptronClassifierExample {
       .appName("MultilayerPerceptronClassifierExample")
       .getOrCreate()
 
+    //Cargar el archivo o dataframe a leer en formato libsvm y declararlo como valor con nombre "data"
     val data = spark.read.format("libsvm")
-      .load("data/mllib/sample_multiclass_classification_data.txt")
+      .load("/home/gussy/git_workspace/Big-Data2020/Unidad2/Practicas/Practica7/sample_multiclass_classification_data.txt")
 
+    //Se dividen los datos en train y test de manera aleatoria (60% para train y 40% para test)
     val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
     val train = splits(0)
     val test = splits(1)
 
-    val layers = Array[Int](4, 5, 4, 3)
+    //Se especifican las capas de la red neuronal en un arreglo
+    val layers = Array[Int](4, 5, 4, 3) //la capa de entrada es de tamaño 4 (caracteristicas), las dos intermedias de tamaño 5 y 4, y la de salida de tamaño 3 (clases)
 
+    //Se declara el clasificador base y se establecen los parametros del entrenamiento
     val trainer = new MultilayerPerceptronClassifier()
       .setLayers(layers)
       .setBlockSize(128)
       .setSeed(1234L)
       .setMaxIter(100)
 
+    //Se inicia el entrenamiento del modelo
     val model = trainer.fit(train)
 
+    //Se calcula la precision de los datos de prueba
     val result = model.transform(test)
     val predictionAndLabels = result.select("prediction", "label")
     val evaluator = new MulticlassClassificationEvaluator()
       .setMetricName("accuracy")
 
+    //Se despliega la exactitud del modelo
     println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
 
     spark.stop()
   }
 }
 
+/* Resultado:
+Test set accuracy = 0.9019607843137255
+*/
--- Unidad2/Practicas/Practica9/Practica9.scala
+++ Unidad2/Practicas/Practica9/Practica9.scala
@@ -1,5 +1,3 @@
-package org.apache.spark.examples.ml
-
 import org.apache.spark.ml.classification.{LogisticRegression, OneVsRest}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
@@ -12,22 +10,29 @@ object OneVsRestExample {
       .appName(s"OneVsRestExample")
       .getOrCreate()
 
+     //Cargar el archivo o dataframe a leer en formato libsvm y declararlo como valor con nombre "inputData"
     val inputData = spark.read.format("libsvm")
-      .load("data/mllib/sample_multiclass_classification_data.txt")
+      .load("/home/gussy/git_workspace/Big-Data2020/Unidad2/Practicas/Practica9/sample_multiclass_classification_data.txt")
 
+    //Se dividen los datos en train y test de manera aleatoria (80% para train y 20% para test)
     val Array(train, test) = inputData.randomSplit(Array(0.8, 0.2))
 
+    //Se declara el clasificador base y se establecen los parametros del entrenamiento
     val classifier = new LogisticRegression()
       .setMaxIter(10)
       .setTol(1E-6)
       .setFitIntercept(true)
 
+    //Se declara el clasficador One vs Rest
     val ovr = new OneVsRest().setClassifier(classifier)
 
+    //Se inicia el entrenamiento del modelo
     val ovrModel = ovr.fit(train)
 
+    //Se realizan las predicciones
     val predictions = ovrModel.transform(test)
 
+    //Se calcula la precision de los datos de prueba
     val evaluator = new MulticlassClassificationEvaluator()
       .setMetricName("accuracy")
 
@@ -36,6 +41,7 @@ object OneVsRestExample {
 
     spark.stop()
   }
-
 }
-
+/* Resultado:
+Test Error = 0.0
+*/
