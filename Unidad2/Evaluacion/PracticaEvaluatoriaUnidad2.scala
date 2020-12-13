70d3d3fc1ea88024b8143f2ca09b0e22df663c4f
GussyDieTwice <gustavo.encarnacion16@tectijuana.edu.mx>
2020-12-13 12:03:31 -0800

    raw evaluation

--- Unidad2/Evaluacion/PracticaEvaluatoriaUnidad2.scala
+++ Unidad2/Evaluacion/PracticaEvaluatoriaUnidad2.scala
@@ -10,7 +10,7 @@ object Evaluacion2 {
     val spark = SparkSession.builder.appName("Evaluacion2").getOrCreate()
 
     val df = spark.read.option("header", "true").option("inferSchema","true")csv("/home/gussy/git_workspace/Big-Data2020/Unidad2/Evaluacion/iris.csv")
-    val df = spark.read.option("header", "true").option("inferSchema","true")csv("C:/Data2020/Big-Data2020/Unidad2/Evaluacion/iris.csv")
+    //val df = spark.read.option("header", "true").option("inferSchema","true")csv("C:/Data2020/Big-Data2020/Unidad2/Evaluacion/iris.csv")
 
     val datos = df.columns
     df.columns
@@ -21,22 +21,22 @@ object Evaluacion2 {
     val desc = df.describe().show()
     df.describe().show() 
 
-    val indexer = new StringIndexer().setInputCol("species").setOutputCol("indexedSpecies").fit(df)
-    val indexed = indexer.transform(df).drop("species").withColumnRenamed("indexedSpecies", "species")
+    val indexer = new StringIndexer().setInputCol("species").setOutputCol("indexedLabel").fit(df)
+    val indexed = indexer.transform(df).drop("species").withColumnRenamed("indexedLabel", "label")
     indexed.show()
 
     val assembler = new VectorAssembler().setInputCols(Array("sepal_length","sepal_width","petal_length","petal_width")).setOutputCol("features")
     val data = assembler.transform(indexed)
     data.show()
 
-    val labelIndexer = new StringIndexer().setInputCol("species").setOutputCol("indexedSpecies").fit(indexed)
+    val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(indexed)
 
     println(s"Labels: ${indexer.labels.mkString("[", ", ", "]")}")
     println(s"indexedLabels: ${labelIndexer.labels.mkString("[", ", ", "]")}")
 
-/*
+
     // Split the data into train and test
-    val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
+    val splits = data.randomSplit(Array(0.7, 0.3), seed = 1234L)
     val train = splits(0)
     val test = splits(1)
 
@@ -46,11 +46,7 @@ object Evaluacion2 {
     val layers = Array[Int](4, 5, 4, 3)
 
     // create the trainer and set its parameters
-    val trainer = new MultilayerPerceptronClassifier()
-      .setLayers(layers)
-      .setBlockSize(128)
-      .setSeed(1234L)
-      .setMaxIter(100)
+    val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
 
     // train the model
     val model = trainer.fit(train)
@@ -58,11 +54,9 @@ object Evaluacion2 {
     // compute accuracy on the test set
     val result = model.transform(test)
     val predictionAndLabels = result.select("prediction", "label")
-    val evaluator = new MulticlassClassificationEvaluator()
-      .setMetricName("accuracy")
+    val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
 
     println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
-    */
 
     spark.stop()
   }
