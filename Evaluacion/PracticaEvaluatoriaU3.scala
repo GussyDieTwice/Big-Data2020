dd58a1b0be8232130b7a5903b5c04c7eede8ef96
AdrianGiovanny <71359267+AdrianGiovanny@users.noreply.github.com>
2020-12-16 20:59:32 -0800

    Commit Practice Eval Unit3

---
+++ Evaluacion/PracticaEvaluatoriaU3.scala
@@ -0,0 +1,27 @@
+//1. Importar una simple sesión Spark.
+import org.apache.spark.sql.SparkSession
+
+//2. Utilice las lineas de código para minimizar errores
+import org.apache.log4j._
+Logger.getLogger("org").setLevel(Level.ERROR)
+
+//3. Cree una instancia de la sesión Spark
+val spark = SparkSession.builder().getOrCreate()
+
+//4. Importar la librería de Kmeans para el algoritmo de agrupamiento.
+import org.apache.spark.ml.clustering.KMeans
+
+//5. Carga el dataset de Wholesale Customers Data
+val df = spark.read.option("header","true").option("inferSchema","true")csv("C:/Data2020/Big-Data2020/Evaluacion/Wholesale customers data.csv")
+df.show
+
+//6. Seleccione las siguientes columnas: Fresh, Milk, Grocery, Frozen, Detergents_Paper, 
+//Delicassen y llamar a este conjunto feature_data
+val  feature_data  = df.select("Fresh","Milk","Grocery","Frozen","Detergents_Paper","Delicassen")
+feature_data.show
+
+//7. Importar Vector Assembler y Vector
+import org.apache.spark.ml.feature.VectorAssembler
+
+// 8. Crea un nuevo objeto Vector Assembler para las columnas de caracteristicas como un conjunto de entrada, recordando que no hay etiquetas
+val assembler = new VectorAssembler().setInputCols(Array("Fresh","Milk","Grocery","Frozen","Detergents_Paper","Delicassen")).setOutputCol("features")
